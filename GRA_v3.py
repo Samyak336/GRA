44ef089d5735ba8e30f0c42769d3ff9fa463d335
Samyak336 <149242603+Samyak336@users.noreply.github.com>
2024-12-16 19:54:26 +0530

    Update GRA_v3.py

--- GRA_v3.py
+++ GRA_v3.py
@@ -54,9 +54,9 @@ tf.flags.DEFINE_integer('image_resize', 331, 'Height of each input images.')
 
 tf.flags.DEFINE_string('checkpoint_path', './models','Path to checkpoint for pretained models.')
 
-tf.flags.DEFINE_string('input_dir', None, 'Input directory with images.')
-tf.flags.DEFINE_string('output_dir', None, 'Output directory with images.')
-tf.flags.DEFINE_string('labels_path', None, 'input csv')
+tf.flags.DEFINE_string('input_dir', './dev_data/val_rs', 'Input directory with images.')
+tf.flags.DEFINE_string('output_dir', './outputs/gra_v3', 'Output directory with images.')
+tf.flags.DEFINE_string('labels_path','./dev_data/val_rs.csv', 'input csv')
 tf.flags.DEFINE_float('eta', 0.94, 'Value for the eta parameter.')  # Set a default value
 ##########
 tf.flags.DEFINE_float('gamma', 0.01,'Value for the gamma parameter.')
@@ -166,30 +166,31 @@ def batch_grad(x, one_hot, i, max_iter, alpha, grad):
     return x, one_hot, i, max_iter, alpha, grad
 
 
-#############
-def adjust_alpha_tensor(cos_sim, alpha, gamma, high_thresh, low_thresh):
-    """Dynamically adjusts alpha based on the cosine similarity."""
-    mean_sim = tf.reduce_mean(cos_sim)
-    adjustment_factor = tf.cond(
-        mean_sim > high_thresh,
-        lambda: 1 - gamma,  # Decrease alpha
-        lambda: tf.cond(
-            mean_sim < low_thresh,
-            lambda: 1 + gamma,  # Increase alpha
-            lambda: 1.0         # No change
-        )
-    )
-    return alpha * adjustment_factor
+############# ( Use this code for dynamic alpha ablation)
+# def adjust_alpha_tensor(cos_sim, alpha, gamma, high_thresh, low_thresh):
+#     """Dynamically adjusts alpha based on the cosine similarity."""
+#     mean_sim = tf.reduce_mean(cos_sim)
+#     adjustment_factor = tf.cond(
+#         mean_sim > high_thresh,
+#         lambda: 1 - gamma,  # Decrease alpha
+#         lambda: tf.cond(
+#             mean_sim < low_thresh,
+#             lambda: 1 + gamma,  # Increase alpha
+#             lambda: 1.0         # No change
+#         )
+#     )
+#     return alpha * adjustment_factor
 ##############
 
 
 def graph(x, y, i, x_max, x_min, grad, samgrad, m): 
     eps = 2.0 * FLAGS.max_epsilon / 255.0
     num_iter = FLAGS.num_iter
-###########
-    gamma = FLAGS.gamma
-    high_thresh = FLAGS.high_thresh
-    low_thresh = FLAGS.low_thresh
+    alpha= eps / num_iter
+###########( Use this code for dynamic alpha ablation)
+    # gamma = FLAGS.gamma
+    # high_thresh = FLAGS.high_thresh
+    # low_thresh = FLAGS.low_thresh
 ###########
     momentum = FLAGS.momentum
     num_classes = 1001
@@ -207,6 +208,9 @@ def graph(x, y, i, x_max, x_min, grad, samgrad, m):
     _, _, _, _, _, global_grad = tf.while_loop(grad_finish, batch_grad, [x, one_hot, iter, max_iter, eps*FLAGS.beta, tf.zeros_like(new_grad)])
     samgrad = global_grad / (1. * FLAGS.number) 
 
+############ ( Use this code for ablation 2)
+    # current_grad = samgrad 
+############
     ## Neighbor Weighted Correction ##    
     cossim = tf.reduce_sum(new_grad * samgrad, [1, 2, 3]) / (tf.sqrt(tf.reduce_sum(new_grad ** 2, [1, 2, 3])) * tf.sqrt(tf.reduce_sum(samgrad ** 2, [1, 2, 3])))
     cossim = tf.expand_dims(cossim, -1)
@@ -214,8 +218,8 @@ def graph(x, y, i, x_max, x_min, grad, samgrad, m):
     cossim = tf.expand_dims(cossim, -1)
     # cossim=tf.maximum(low,cossim)
     current_grad = cossim*new_grad + (1-cossim)*samgrad  
-############
-    alpha = adjust_alpha_tensor(tf.reduce_mean(cossim), eps / num_iter, gamma, high_thresh, low_thresh)
+############ ( Use this code for dynamic alpha ablation)
+    # alpha = adjust_alpha_tensor(tf.reduce_mean(cossim), eps / num_iter, gamma, high_thresh, low_thresh)
 ############
     noiselast = grad
     noise = momentum * grad + (current_grad) / tf.reduce_mean(tf.abs(current_grad), [1, 2, 3], keep_dims=True)
@@ -234,39 +238,39 @@ def stop(x, y, i, x_max, x_min, grad, samgrad, m):
     num_iter = FLAGS.num_iter
     return tf.less(i, num_iter)
 
-
-def image_augmentation(x):
-    # img, noise
-    one = tf.fill([tf.shape(x)[0], 1], 1.)
-    zero = tf.fill([tf.shape(x)[0], 1], 0.)
-    transforms = tf.concat([one, zero, zero, zero, one, zero, zero, zero], axis=1)
-    rands = tf.concat([tf.truncated_normal([tf.shape(x)[0], 6], stddev=0.05), zero, zero], axis=1)
-    return images_transform(x, transforms + rands, interpolation='BILINEAR')
-
-
-def image_rotation(x):
-    """ imgs, scale, scale is in radians """
-    rands = tf.truncated_normal([tf.shape(x)[0]], stddev=0.05)
-    return images_rotate(x, rands, interpolation='BILINEAR')
+############################################### (Use this code for GRA-CT)
+# def image_augmentation(x):
+#     # img, noise
+#     one = tf.fill([tf.shape(x)[0], 1], 1.)
+#     zero = tf.fill([tf.shape(x)[0], 1], 0.)
+#     transforms = tf.concat([one, zero, zero, zero, one, zero, zero, zero], axis=1)
+#     rands = tf.concat([tf.truncated_normal([tf.shape(x)[0], 6], stddev=0.05), zero, zero], axis=1)
+#     return images_transform(x, transforms + rands, interpolation='BILINEAR')
 
 
-def input_diversity(input_tensor):
-    rnd = tf.random_uniform((), FLAGS.image_width, FLAGS.image_resize, dtype=tf.int32)
-    rescaled = tf.image.resize_images(input_tensor, [rnd, rnd], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-    h_rem = FLAGS.image_resize - rnd
-    w_rem = FLAGS.image_resize - rnd
-    pad_top = tf.random_uniform((), 0, h_rem, dtype=tf.int32)
-    pad_bottom = h_rem - pad_top
-    pad_left = tf.random_uniform((), 0, w_rem, dtype=tf.int32)
-    pad_right = w_rem - pad_left
-    padded = tf.pad(rescaled, [[0, 0], [pad_top, pad_bottom], [pad_left, pad_right], [0, 0]], constant_values=0.)
-    padded.set_shape((input_tensor.shape[0], FLAGS.image_resize, FLAGS.image_resize, 3))
-    ret = tf.cond(tf.random_uniform(shape=[1])[0] < tf.constant(FLAGS.prob), lambda: padded, lambda: input_tensor)
-    ret = tf.image.resize_images(ret, [FLAGS.image_height, FLAGS.image_width],
-                                 method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-    return ret
-    # return tf.cond(tf.random_uniform(shape=[1])[0] < tf.constant(FLAGS.prob), lambda: padded, lambda: input_tensor)
+# def image_rotation(x):
+#     """ imgs, scale, scale is in radians """
+#     rands = tf.truncated_normal([tf.shape(x)[0]], stddev=0.05)
+#     return images_rotate(x, rands, interpolation='BILINEAR')
 
+
+# def input_diversity(input_tensor):
+#     rnd = tf.random_uniform((), FLAGS.image_width, FLAGS.image_resize, dtype=tf.int32)
+#     rescaled = tf.image.resize_images(input_tensor, [rnd, rnd], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+#     h_rem = FLAGS.image_resize - rnd
+#     w_rem = FLAGS.image_resize - rnd
+#     pad_top = tf.random_uniform((), 0, h_rem, dtype=tf.int32)
+#     pad_bottom = h_rem - pad_top
+#     pad_left = tf.random_uniform((), 0, w_rem, dtype=tf.int32)
+#     pad_right = w_rem - pad_left
+#     padded = tf.pad(rescaled, [[0, 0], [pad_top, pad_bottom], [pad_left, pad_right], [0, 0]], constant_values=0.)
+#     padded.set_shape((input_tensor.shape[0], FLAGS.image_resize, FLAGS.image_resize, 3))
+#     ret = tf.cond(tf.random_uniform(shape=[1])[0] < tf.constant(FLAGS.prob), lambda: padded, lambda: input_tensor)
+#     ret = tf.image.resize_images(ret, [FLAGS.image_height, FLAGS.image_width],
+#                                  method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+#     return ret
+    ## return tf.cond(tf.random_uniform(shape=[1])[0] < tf.constant(FLAGS.prob), lambda: padded, lambda: input_tensor)
+###############################################
 
 def main(_):
     # Images for inception classifier are normalized to be in [-1, 1] interval,
@@ -319,6 +323,16 @@ def main(_):
             for filenames, images in load_images(FLAGS.input_dir, batch_shape):
                 idx = idx + 1
                 print("start the i={} attack".format(idx))
+########################################## (Use this code for GRA-CT)
+                # # Convert images to a tensor
+                # images_tensor = tf.convert_to_tensor(images, dtype=tf.float32)
+                
+                # # Apply augmentation
+                # images_tensor = image_augmentation(images_tensor)
+                # images_tensor = image_rotation(images_tensor)
+                # images_tensor = input_diversity(images_tensor)
+                # adv_images = sess.run(x_adv, feed_dict={x_input: images_tensor.eval()})
+##########################################        
 
                 adv_images = sess.run(x_adv, feed_dict={x_input: images})
                 save_images(adv_images, filenames, FLAGS.output_dir)
